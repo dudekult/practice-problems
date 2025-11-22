638ac4fdb226c000f94e70533337cde8ceea41ba
Tushar Babu Dudekula <126271608+dudekult@users.noreply.github.com>
2025-11-22 16:25:33 +0530

    Adding Day1 Python files

---
+++ Day1-Helloworld/Python_Indentation.py
@@ -0,0 +1,9 @@
+
+# Python Indentation refers to the spaces at the beginning of a code line.
+# In other languages indentation is only for readability,
+# but in Python indentation is used to define blocks of code.
+
+if 5 > 10:
+    print("Hello, Five is greater than Ten")
+else:
+    print("Hello, Five is not greater than Ten")

da75e7447dd192dc4545ce4d457cedc5ac2f3bf8
Tushar Babu Dudekula <126271608+dudekult@users.noreply.github.com>
2025-11-22 17:10:02 +0530

    new classes are added please check

--- Day1-Helloworld/Python_Indentation.py
+++ Day1-Helloworld/Python_Indentation.py
@@ -1,9 +1,14 @@
 
 # Python Indentation refers to the spaces at the beginning of a code line.
-# In other languages indentation is only for readability,
-# but in Python indentation is used to define blocks of code.
+# Python indentation is used to define blocks of code.
+#In Python, indentation is not just for readability — it defines :
+#1.the structure of the code. 
+#2.Blocks like loops, conditionals, functions, and classes must be indented consistently.
 
 if 5 > 10:
     print("Hello, Five is greater than Ten")
 else:
     print("Hello, Five is not greater than Ten")
+    print("Hello, Five is not greater than Ten")
+
+
---
+++ Day1-Helloworld/pythonstatements.py
@@ -0,0 +1,6 @@
+#python statement :- A statement is a single instruction that Python can execute print("Hello")
+#types
+#1.Assignment Statements:-
+x = 10
+name = "John"
+

76b8c2c446c701a44d18dca84dc4f87e3b25a088
Tushar Babu Dudekula <126271608+dudekult@users.noreply.github.com>
2025-11-24 10:56:20 +0530

    New statements loops classes are added

--- Day1-Helloworld/Looping_through_a_ist.py
+++ Day1-Helloworld/Looping_through_a_ist.py
@@ -1,7 +1,12 @@
 Companies = ["ADP","TCS","WIPRO"]
-
+'''
 for names in Companies:
     if 'A' in names:
         print(names)
     else:
         print("No matching not found")
+'''
+
+print("=== With numbers ===")
+for i, company in enumerate(Companies, start=1):
+    print(f"{i}. {company}")
